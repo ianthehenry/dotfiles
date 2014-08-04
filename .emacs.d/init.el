1c39528e2e2fbe5af89618a21479fae546a97f10
Ian Henry <ianthehenry@gmail.com>
2014-08-03 23:47:13 -0400

    emacs packages and colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,6 @@
-;; Cask!
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+(require 'pallet)
 
 (defvar dotfiles-dir (file-name-directory
                       (or (buffer-file-name) load-file-name)))
@@ -11,7 +11,23 @@
 (load-library "better-defaults")
 (load-library "init-packages")
 
+(evil-mode)
+(evil-surround-mode)
+
 (set-keyboard-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 (windmove-default-keybindings)
 (fset 'yes-or-no-p 'y-or-n-p)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
